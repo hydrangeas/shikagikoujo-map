b344aeebd55d0b8dfee8d42acc8a38e863786936
Akira Otaka <hydrangeas.project@gmail.com>
2024-10-15 23:48:18 +0900

    青森県全域を追加

---
+++ misc/aomoriken.js
@@ -0,0 +1,125 @@
+"use strict";
+var __awaiter = (this && this.__awaiter) || function (thisArg, _arguments, P, generator) {
+    function adopt(value) { return value instanceof P ? value : new P(function (resolve) { resolve(value); }); }
+    return new (P || (P = Promise))(function (resolve, reject) {
+        function fulfilled(value) { try { step(generator.next(value)); } catch (e) { reject(e); } }
+        function rejected(value) { try { step(generator["throw"](value)); } catch (e) { reject(e); } }
+        function step(result) { result.done ? resolve(result.value) : adopt(result.value).then(fulfilled, rejected); }
+        step((generator = generator.apply(thisArg, _arguments || [])).next());
+    });
+};
+var __generator = (this && this.__generator) || function (thisArg, body) {
+    var _ = { label: 0, sent: function() { if (t[0] & 1) throw t[1]; return t[1]; }, trys: [], ops: [] }, f, y, t, g = Object.create((typeof Iterator === "function" ? Iterator : Object).prototype);
+    return g.next = verb(0), g["throw"] = verb(1), g["return"] = verb(2), typeof Symbol === "function" && (g[Symbol.iterator] = function() { return this; }), g;
+    function verb(n) { return function (v) { return step([n, v]); }; }
+    function step(op) {
+        if (f) throw new TypeError("Generator is already executing.");
+        while (g && (g = 0, op[0] && (_ = 0)), _) try {
+            if (f = 1, y && (t = op[0] & 2 ? y["return"] : op[0] ? y["throw"] || ((t = y["return"]) && t.call(y), 0) : y.next) && !(t = t.call(y, op[1])).done) return t;
+            if (y = 0, t) op = [op[0] & 2, t.value];
+            switch (op[0]) {
+                case 0: case 1: t = op; break;
+                case 4: _.label++; return { value: op[1], done: false };
+                case 5: _.label++; y = op[1]; op = [0]; continue;
+                case 7: op = _.ops.pop(); _.trys.pop(); continue;
+                default:
+                    if (!(t = _.trys, t = t.length > 0 && t[t.length - 1]) && (op[0] === 6 || op[0] === 2)) { _ = 0; continue; }
+                    if (op[0] === 3 && (!t || (op[1] > t[0] && op[1] < t[3]))) { _.label = op[1]; break; }
+                    if (op[0] === 6 && _.label < t[1]) { _.label = t[1]; t = op; break; }
+                    if (t && _.label < t[2]) { _.label = t[2]; _.ops.push(op); break; }
+                    if (t[2]) _.ops.pop();
+                    _.trys.pop(); continue;
+            }
+            op = body.call(thisArg, _);
+        } catch (e) { op = [6, e]; y = 0; } finally { f = t = 0; }
+        if (op[0] & 5) throw op[1]; return { value: op[0] ? op[1] : void 0, done: true };
+    }
+};
+Object.defineProperty(exports, "__esModule", { value: true });
+var fs = require("fs");
+var csv = require("csv-parser");
+var csv_writer_1 = require("csv-writer");
+var inputFilePath = 'aomoriken-input.csv';
+var outputFilePath = 'aomoriken.csv';
+var apiKey = 'AIzaSyDmoY5C-Ltr_eDT8KC0_AwZ-ToL8SMn380';
+var apiUrl = 'https://maps.googleapis.com/maps/api/geocode/json';
+var facilities = [];
+fs.createReadStream(inputFilePath)
+    .pipe(csv())
+    .on('data', function (row) {
+    facilities.push({
+        管轄: row['管轄'],
+        'No.': row['No.'],
+        区分: row['区分'],
+        名称: row['名称'],
+        郵便番号: row['郵便番号'],
+        市町村名: row['市町村名'],
+        所在地: row['所在地'],
+        保健所: row['保健所'],
+        開設者: row['開設者'],
+        管理者: row['管理者'],
+    });
+})
+    .on('end', function () { return __awaiter(void 0, void 0, void 0, function () {
+    var outputFacilities, _i, facilities_1, facility, address, response, data, location_1, i, result, csvWriter;
+    var _a;
+    return __generator(this, function (_b) {
+        switch (_b.label) {
+            case 0:
+                outputFacilities = [];
+                _i = 0, facilities_1 = facilities;
+                _b.label = 1;
+            case 1:
+                if (!(_i < facilities_1.length)) return [3 /*break*/, 5];
+                facility = facilities_1[_i];
+                address = "\u9752\u68EE\u770C".concat(facility.市町村名).concat(facility.所在地);
+                return [4 /*yield*/, fetch("".concat(apiUrl, "?key=").concat(apiKey, "&address=").concat(encodeURIComponent("\u3012".concat(facility.郵便番号, " ").concat(address))))];
+            case 2:
+                response = _b.sent();
+                return [4 /*yield*/, response.json()];
+            case 3:
+                data = _b.sent();
+                location_1 = (_a = data.results[0]) === null || _a === void 0 ? void 0 : _a.geometry.location;
+                for (i = 1; i < data.results.length; i++) {
+                    console.log("0 ".concat(facility.名称, " (").concat(address, "), ").concat(location_1.lat, ", ").concat(location_1.lng, ",").concat(data.results[0].geometry.location_type));
+                    result = data.results[i];
+                    console.log("".concat(i, " ").concat(facility.名称, " (").concat(address, "), ").concat(result.geometry.location.lat, ", ").concat(result.geometry.location.lng, ", ").concat(result.geometry.location_type));
+                    if (result.geometry.location_type === 'ROOFTOP' || result.geometry.location_type === 'APPROXIMATE') {
+                        location_1 = result.geometry.location;
+                    }
+                }
+                outputFacilities.push({
+                    管轄: facility.管轄,
+                    管理番号: facility['No.'],
+                    名称: facility.名称,
+                    緯度: location_1 ? location_1.lat : null,
+                    経度: location_1 ? location_1.lng : null,
+                    所在地: address,
+                    電話番号: ''
+                });
+                _b.label = 4;
+            case 4:
+                _i++;
+                return [3 /*break*/, 1];
+            case 5:
+                csvWriter = (0, csv_writer_1.createObjectCsvWriter)({
+                    path: outputFilePath,
+                    header: [
+                        { id: '管轄', title: '管轄' },
+                        { id: '管理番号', title: '管理番号' },
+                        { id: '名称', title: '名称' },
+                        { id: '緯度', title: '緯度' },
+                        { id: '経度', title: '経度' },
+                        { id: '所在地', title: '所在地' },
+                        { id: '電話番号', title: '電話番号' }
+                    ]
+                });
+                return [4 /*yield*/, csvWriter.writeRecords(outputFacilities)];
+            case 6:
+                _b.sent();
+                console.log('aomoriken.csv has been written successfully.');
+                return [2 /*return*/];
+        }
+    });
+}); });
+// npx tsc aomoriken.ts && node aomoriken.js | tee aomoriken.log
---
+++ misc/aomoriken.ts
@@ -0,0 +1,96 @@
+import * as fs from 'fs';
+import * as csv from 'csv-parser';
+import { createObjectCsvWriter } from 'csv-writer';
+
+const inputFilePath = 'aomoriken-input.csv';
+const outputFilePath = 'aomoriken.csv';
+const apiKey = 'AIzaSyDmoY5C-Ltr_eDT8KC0_AwZ-ToL8SMn380';
+const apiUrl = 'https://maps.googleapis.com/maps/api/geocode/json';
+
+interface Facility {
+  管轄: string;
+  'No.': string;
+  区分: string;
+  名称: string;
+  郵便番号: string;
+  市町村名: string;
+  所在地: string;
+  保健所: string;
+  開設者: string;
+  管理者: string;
+}
+
+interface OutputFacility {
+  管轄: string;
+  管理番号: string;
+  名称: string;
+  緯度: number | null;
+  経度: number | null;
+  所在地: string;
+  電話番号: string;
+}
+
+const facilities: Facility[] = [];
+
+fs.createReadStream(inputFilePath)
+  .pipe(csv())
+  .on('data', (row) => {
+    facilities.push({
+      管轄: row['管轄'],
+      'No.': row['No.'],
+      区分: row['区分'],
+      名称: row['名称'],
+      郵便番号: row['郵便番号'],
+      市町村名: row['市町村名'],
+      所在地: row['所在地'],
+      保健所: row['保健所'],
+      開設者: row['開設者'],
+      管理者: row['管理者'],
+    });
+  })
+  .on('end', async () => {
+    const outputFacilities: OutputFacility[] = [];
+
+    for (const facility of facilities) {
+      const address = `青森県${facility.市町村名}${facility.所在地}`;
+      const response = await fetch(`${apiUrl}?key=${apiKey}&address=${encodeURIComponent(`〒${facility.郵便番号} ${address}`)}`);
+      const data = await response.json();
+
+      let location = data.results[0]?.geometry.location;
+      for (let i = 1; i < data.results.length; i++) {
+        console.log(`0 ${facility.名称} (${address}), ${location.lat}, ${location.lng},${data.results[0].geometry.location_type}`);
+        const result = data.results[i];
+        console.log(`${i} ${facility.名称} (${address}), ${result.geometry.location.lat}, ${result.geometry.location.lng}, ${result.geometry.location_type}`);
+        if (result.geometry.location_type === 'ROOFTOP' || result.geometry.location_type === 'APPROXIMATE') {
+          location = result.geometry.location;
+        }
+      }
+
+      outputFacilities.push({
+        管轄: facility.管轄,
+        管理番号: facility['No.'],
+        名称: facility.名称,
+        緯度: location ? location.lat : null,
+        経度: location ? location.lng : null,
+        所在地: address,
+        電話番号: ''
+      });
+    }
+
+    const csvWriter = createObjectCsvWriter({
+      path: outputFilePath,
+      header: [
+        { id: '管轄', title: '管轄' },
+        { id: '管理番号', title: '管理番号' },
+        { id: '名称', title: '名称' },
+        { id: '緯度', title: '緯度' },
+        { id: '経度', title: '経度' },
+        { id: '所在地', title: '所在地' },
+        { id: '電話番号', title: '電話番号' }
+      ]
+    });
+
+    await csvWriter.writeRecords(outputFacilities);
+    console.log('aomoriken.csv has been written successfully.');
+  });
+// npx tsc aomoriken.ts && node aomoriken.js | tee aomoriken.log
